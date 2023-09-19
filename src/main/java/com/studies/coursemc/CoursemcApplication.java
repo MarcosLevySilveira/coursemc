5e16bbad92790432a5200a356a7c04f99c9e928e
MarcosLevySilveira <marcoslevy_silveira@hotmail.com>
2023-09-19 09:46:25 -0400

    Product association ManyToMany

--- src/main/java/com/studies/coursemc/CoursemcApplication.java
+++ src/main/java/com/studies/coursemc/CoursemcApplication.java
@@ -8,11 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.studies.coursemc.domain.Category;
+import com.studies.coursemc.domain.Product;
 import com.studies.coursemc.repositories.CategoryRepository;
+import com.studies.coursemc.repositories.ProductRepository;
 
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner{
 
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
@@ -27,8 +33,21 @@ public class CoursemcApplication implements CommandLineRunner{
 		Category cat1 = new Category(null,"Computing");
 		Category cat2 = new Category(null,"Office");
 		
-		categoryRepository.saveAll((Arrays.asList(cat1,cat2)));
+		Product p1 = new Product(null,"Computer",2000.0);
+		Product p2 = new Product(null,"Printer",800.0);
+		Product p3 = new Product(null,"Mouse",80.0);
 		
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		
+		categoryRepository.saveAll((Arrays.asList(cat1,cat2)));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
