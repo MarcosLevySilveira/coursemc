c957fead8fcd5c01a171bfaf24a2a2595a31ca31
MarcosLevySilveira <marcoslevy_silveira@hotmail.com>
2023-09-19 07:50:34 -0400

    Created project

---
+++ src/test/java/com/studies/coursemc/CoursemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.coursemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
